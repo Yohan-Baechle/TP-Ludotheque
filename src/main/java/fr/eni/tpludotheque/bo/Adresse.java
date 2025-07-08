613ca934ea4da1d3381ae11bab2cfc43e5ae662a
Yohan-Baechle <baechle.yohan@gmail.com>
2025-07-08 16:28:42 +0200

    création de l'entité Adresse

---
+++ src/main/java/fr/eni/tpludotheque/bo/Adresse.java
@@ -0,0 +1,29 @@
+package fr.eni.tpludotheque.bo;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "ADRESSE")
+public class Adresse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "no_adresse")
+    private Integer noAdresse;
+
+    @NonNull
+    @Column(nullable = false, length = 100)
+    private String rue;
+
+    @NonNull
+    @Column(nullable = false, length = 10)
+    private String codePostal;
+
+    @NonNull
+    @Column(nullable = false, length = 50)
+    private String ville;
+}
