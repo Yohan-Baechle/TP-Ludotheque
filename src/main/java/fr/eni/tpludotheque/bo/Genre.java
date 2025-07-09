0b91e7bc460ea2b28aee8188eb63dd04093114a2
Yohan Baechlé <baechle.yohan@gmail.com>
2025-07-09 14:01:06 +0200

    chore: création de l'objet métier Genre

---
+++ src/main/java/fr/eni/tpludotheque/bo/Genre.java
@@ -0,0 +1,20 @@
+package fr.eni.tpludotheque.bo;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "GENRE")
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NonNull
+    @Column(nullable = false, length = 50)
+    private String libelle;
+}
