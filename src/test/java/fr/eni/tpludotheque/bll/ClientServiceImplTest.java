89fe6f709dc3aa070ceca0268970f390939608d2
Yohan-Baechle <baechle.yohan@gmail.com>
2025-07-10 09:56:54 +0200

    création du test mocké ClientServiceImplTest

--- src/test/java/fr/eni/tpludotheque/bll/ClientServiceImplTest.java
+++ src/test/java/fr/eni/tpludotheque/bll/ClientServiceImplTest.java
@@ -14,7 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(MockitoExtension.class)
-public class ClientServiceImplTest {
+public class ClientServiceImplCTest {
 
     @Mock
     private ClientRepository clientRepository;
