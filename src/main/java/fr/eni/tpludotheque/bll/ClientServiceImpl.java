a9cf2cfd9c0dd9ec1406a41899291cba61a594ae
Yohan Baechlé <baechle.yohan@gmail.com>
2025-07-10 09:52:18 +0200

    feat: injection par constructeur ajoutée à ClientServiceImpl

--- src/main/java/fr/eni/tpludotheque/bll/ClientServiceImpl.java
+++ src/main/java/fr/eni/tpludotheque/bll/ClientServiceImpl.java
@@ -2,14 +2,17 @@ package fr.eni.tpludotheque.bll;
 
 import fr.eni.tpludotheque.bo.Client;
 import fr.eni.tpludotheque.dal.ClientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientServiceImpl implements ClientService {
 
-    @Autowired
-    private ClientRepository clientRepository;
+    private final ClientRepository clientRepository;
+
+    // Injection par constructeur
+    public ClientServiceImpl(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
 
     @Override
     public Client ajouterClient(Client client) {
