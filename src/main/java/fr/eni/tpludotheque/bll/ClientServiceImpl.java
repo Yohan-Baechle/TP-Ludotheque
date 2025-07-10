2fa4755c01f6f1fed34902a9d9e3a423aa46ba46
Yohan-Baechle <baechle.yohan@gmail.com>
2025-07-10 09:50:50 +0200

    création de ClientServiceImpl

--- src/main/java/fr/eni/tpludotheque/bll/ClientServiceImpl.java
+++ src/main/java/fr/eni/tpludotheque/bll/ClientServiceImpl.java
@@ -1,4 +1,18 @@
 package fr.eni.tpludotheque.bll;
 
-public class ClientServiceImpl {
+import fr.eni.tpludotheque.bo.Client;
+import fr.eni.tpludotheque.dal.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientServiceImpl implements ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Override
+    public Client ajouterClient(Client client) {
+        return clientRepository.save(client);
+    }
 }
